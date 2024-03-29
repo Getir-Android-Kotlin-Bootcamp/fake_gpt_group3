382fd902fc3dd76798d54c56bc4fa179eb6a38d6
erenkaraboga <eren.karaboga@naylalabs.com>
2024-03-29 22:29:25 +0300

    Add message ui : wip

---
+++ app/src/main/java/com/week2/getir/fake_gpt/ChatFragment.kt
@@ -0,0 +1,31 @@
+package com.week2.getir.fake_gpt
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChatFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+    companion object {
+        fun createSimpleIntent(context: Context): Intent =
+            Intent(context, ChatFragment::class.java)
+    }
+}
