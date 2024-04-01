21143de3a9026162cbb84ab2621651c962ff2aa4
Muhammet Çağrı Yılmaz <yilmaz004@outlook.com>
2024-04-01 03:57:36 +0300

    Verilen Arama tasarımı yapıldı

--- app/src/main/java/com/week2/getir/fake_gpt/ChatFragment.kt
+++ app/src/main/java/com/week2/getir/fake_gpt/ChatFragment.kt
@@ -7,10 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.week2.getir.fake_gpt.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
 
-
+    private var _binding: FragmentChatBinding ?= null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,12 +22,33 @@ class ChatFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        _binding = FragmentChatBinding.inflate(layoutInflater,container,false)
+        binding.buttonEnter.setOnClickListener {
+            val questions = binding.editTextSearch.text.toString()
+            if(checkConditions(questions)){
+                // Gerekli işlemler yapılacak
+
+
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun checkConditions(questions: String): Boolean {
+        if(questions.toString().isNullOrEmpty()){
+            return false
+        }
+        return true
     }
 
     companion object {
         fun createSimpleIntent(context: Context): Intent =
             Intent(context, ChatFragment::class.java)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
